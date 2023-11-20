8d51c1c518917ec3e1e078bedf105b29c12d99b3
jhojocor <jhojocor@gmail.com>
2023-11-20 17:11:13 -0500

    idk

--- Lab 3/src/user/hooks/useUserApp.js
+++ Lab 3/src/user/hooks/useUserApp.js
@@ -1,59 +1,102 @@
-import { useCallback, useEffect, useContext } from 'react'
-import { fetchMovies } from '../services/fetchMovies'
-import { UserContext } from '../context/UserContex'
+import { useCallback, useContext, useEffect } from 'react';
+import { fetchData } from '../services/fetchData.js';
+import { UserContext } from '../context/UserContext.js';
+import { fetchFact } from '../pages/HomePage/HomePage';
 
-export const useUserApp = () => {
-  const context = useContext(UserContext)
+export function useFilter() {
+  const context = useContext(UserContext);
 
-  if (!context) {
-    throw new Error('This component sould be within a UserContextrovider Component')
-  }
   const {
     error,
-      setError,
-      Movies,
-      setMovies,
-      selectedCategory,
-      setSelectedCategory,
-      isLoading,
-      setIsLoading
-  } = context
+    setError,
+    Movies,
+    setMovies,
+    selectedCategory,
+    setSelectedCategory,
+    isLoading,
+    setIsLoading,
+    endpoint,
+    setEndpoint,
+    query,
+    setQuery,
+    results,
+    setResults
+  } = context;
+
+  if (!context) {
+    throw new Error('This component should be within a UserContextProvider Component');
+  }
 
   // Use effect
-
   useEffect(() => {
-    console.log('useApp is running')
-    getMovies()
-  }, [getMovies])
+    console.log('useFilter is running');
+    handleFetchAndState();
+  }, [endpoint, query]);
 
   // Fetching
+  const handleFetchAndState = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const responseData = await fetchData(endpoint, query);
+      const filteredData = responseData.data.filter((item) =>
+        item.attributes.title.toLowerCase().includes(query.toLowerCase())
+      );
+      setResults(filteredData);
+      setError(null); // Clear error if the request is successful
+    } catch (error) {
+      setError(`Error al obtener datos: ${error.message}`);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [endpoint, query, setResults, setError, setIsLoading]);
 
+  // Fetching movies from the new source
   const getMovies = useCallback(() => {
     setIsLoading(true);
-    fetchMovies()
+    fetchFact()
       .then((newMovies) => {
         setMovies(newMovies);
-        setError(null); // Limpiar el error si la solicitud es exitosa
+        setError(null); // Clear error if the request is successful
       })
       .catch((e) => setError(e))
       .finally(() => setIsLoading(false));
   }, [setMovies, setError, setIsLoading]);
 
-  // Filter Movies
+  // Use effect to fetch movies
+  useEffect(() => {
+    console.log('useFilter is running');
+    getMovies();
+  }, [getMovies]);
 
-  const movieFilter = selectedCategory === 'All'
-  ? Movies
-  : Movies.filter((movie) => {
-    // Si " ", pertenece a "Other"
-    return selectedCategory === 'Other' ? !movie.rt_score : movie.rt_score === selectedCategory;
-  });
-  return (
-    {
-      error,
-      movieFilter,
-      selectedCategory,
-      setSelectedCategory,
-      isLoading
-    }
-  )
+  const handleSelectChange = useCallback(
+    (e) => {
+      const selectedValue = e.target.value;
+      setEndpoint(selectedValue);
+      setQuery('');
+    },
+    [setEndpoint, setQuery]
+  );
+
+  const handleQueryChange = useCallback(
+    (e) => {
+      setQuery(e.target.value);
+    },
+    [setQuery]
+  );
+
+  return {
+    error,
+    Movies,
+    selectedCategory,
+    setSelectedCategory,
+    isLoading,
+    endpoint,
+    setEndpoint,
+    query,
+    setQuery,
+    results,
+    setResults,
+    handleSelectChange,
+    handleQueryChange,
+  };
 }
